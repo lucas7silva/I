5ac80a0913f930e03f4e50d02b07d3d9e2041cb4
lucas7silva <141294696+lucas7silva@users.noreply.github.com>
2023-08-04 01:22:30 -0300

    Add files via upload

---
+++ formulario/java.js
@@ -0,0 +1,8 @@
+function calcular (){
+    var peso=document.getElementById("peso-n")
+    var altura=document.getElementById("altura")
+  
+    var calcular = peso-n / altura **2;
+    console.log(calcular);
+}
+
